d31b3f4e07e8541248afcbfc2fd4de5d5d55cadd
Reahana <reahana233@gmail.com>
2023-07-14 21:32:51 +0600

    save cart

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -7,7 +7,7 @@ const Cart = ({cart}) => {
 
    let totalPrice = 0;
    let totalShipping = 0;
-   let quantity = 0;
+   //let quantity = 0;
    for (const product of cart) {
     // if(product.quantity === 0){
     //     product.quantity = 1;
@@ -16,14 +16,14 @@ const Cart = ({cart}) => {
 
     totalPrice = totalPrice + product.price ;
      totalShipping = totalShipping + product.shipping;
-    // quantity = quantity + product.quantity;
+     //quantity = quantity + product.quantity;
 }
 const tax = totalPrice * 7 / 100;
 const grandTotal = totalPrice + totalShipping + tax;
     return (
         <div className='cart'>
              <h4>Order Summary</h4>
-             <p>Selected Items: {quantity}</p>
+             <p>Selected Items: {cart.length}</p>
              <p>Total Price: ${totalPrice}</p>
             <p>Shipping: ${totalShipping}</p>
             <p>Tax: ${tax.toFixed(2)}</p>
--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import './Shop.css';
 import Product from '../Product/Product';
 import Cart from '../Cart/Cart';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 
 
 const Shop = () => {
@@ -15,10 +16,24 @@ const Shop = () => {
         .then(data => setProducts(data))
     },[])
 
+    useEffect(()=>{
+        const storedCart = getShoppingCart();
+         // step 1: get id of the addedProduct
+        for(const id in storedCart){
+
+        }
+        // step 1: get id of the addedProduct
+        // step 2: get product from products state by using id
+        // step 3: add quantity
+        // step 4: add the added product to the saved cart
+         // step 5: set the cart
+    },[])
+
     const handleAddToCart = (product) => {
         // cart.push(product); 
         const newCart = [...cart, product];
         setCart(newCart);
+        addToDb(product.id);
     
      }
     return (
